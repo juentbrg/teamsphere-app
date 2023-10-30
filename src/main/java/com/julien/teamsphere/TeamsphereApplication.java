9d12f3e4d7591205ef1eaceee2aa3420803b01c0
Julien <julient.bourgeois@gmail.com>
2023-10-30 16:29:32 +0100

    first commit

---
+++ src/main/java/com/julien/teamsphere/TeamsphereApplication.java
@@ -0,0 +1,13 @@
+package com.julien.teamsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julien/teamsphere/TeamsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julien.teamsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
